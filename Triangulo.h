e21c2638ebed49c84b9cb7608cb3ffead13d0fc4
Diego Pacheco <diego@M-1902.local>
2020-03-26 18:50:08 -0600

    Commit de Diego

---
+++ Triangulo.h
@@ -0,0 +1,23 @@
+//
+// Created by Javier Fuentes Mora on 11/02/20.
+//
+
+#ifndef TRIANGULO_H
+#define PTRIANGULO_H
+
+
+#include <vector>
+#include "Coordenada.h"
+
+class PoligonoIrregular {
+private:
+  Coordenada uno;
+  Coordenada dos;
+  Coordenada tres;
+
+public:
+    Triangulo();
+    void imprimeEsq();
+};
+
+#endif //PRACTICA5_POLIGONOIRREGULAR_H
